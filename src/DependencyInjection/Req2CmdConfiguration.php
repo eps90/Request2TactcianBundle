aec4eb1e6aa1c80bc03dd76ea17a5396b061e489
Kuba Turek <ja@kubaturek.pl>
2017-06-23 14:50:42 +0200

    [#6] Split configuration tree onto separate methods

--- src/DependencyInjection/Req2CmdConfiguration.php
+++ src/DependencyInjection/Req2CmdConfiguration.php
@@ -3,12 +3,10 @@ declare(strict_types=1);
 
 namespace Eps\Req2CmdBundle\DependencyInjection;
 
+use Symfony\Component\Config\Definition\Builder\NodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
-/**
- * @todo Rename
- */
 final class Req2CmdConfiguration implements ConfigurationInterface
 {
     /**
@@ -22,29 +20,45 @@ final class Req2CmdConfiguration implements ConfigurationInterface
         $root = $builder->root('req2cmd');
         $root
             ->children()
-                ->arrayNode('extractor')
-                    ->addDefaultsIfNotSet()
-                    ->beforeNormalization()
-                        ->ifString()
-                        ->then(function ($extractorName) {
-                            return ['service_id' => 'eps.req2cmd.extractor.' . $extractorName];
-                        })
-                    ->end()
-                    ->children()
-                        ->scalarNode('service_id')
-                            ->cannotBeEmpty()
-                            ->defaultValue('eps.req2cmd.extractor.serializer')
-                        ->end()
-                    ->end()
-                ->end()
-                ->arrayNode('param_mappers')
-                    ->defaultValue(['eps.req2cmd.param_mapper.path'])
-                    ->prototype('scalar')
-                        ->cannotBeEmpty()
-                    ->end()
-                ->end()
+                ->append($this->addExtractorNode())
+                ->append($this->addParamMappersNode())
             ->end();
 
         return $builder;
+    }
+
+    private function addExtractorNode(): NodeDefinition
+    {
+        $builder = new TreeBuilder();
+        $root = $builder->root('extractor');
+        $root
+            ->addDefaultsIfNotSet()
+            ->beforeNormalization()
+                ->ifString()
+                ->then(function ($extractorName) {
+                    return ['service_id' => 'eps.req2cmd.extractor.' . $extractorName];
+                })
+            ->end()
+            ->children()
+                ->scalarNode('service_id')
+                    ->cannotBeEmpty()
+                    ->defaultValue('eps.req2cmd.extractor.serializer')
+                ->end()
+            ->end();
+
+        return $root;
+    }
+
+    private function addParamMappersNode(): NodeDefinition
+    {
+        $builder = new TreeBuilder();
+        $node = $builder->root('param_mappers');
+        $node
+            ->defaultValue(['eps.req2cmd.param_mapper.path'])
+            ->prototype('scalar')
+                ->cannotBeEmpty()
+            ->end();
+
+        return $node;
     }
 }
